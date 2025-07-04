485b6846d1bcad6c20e06c020421f5f3cb089611
aditya-pushkar <adityapushkar850@gmail.com>
2025-07-05 03:32:14 +0530

    aiChat, added the diffrenciate the user and admin field

--- src/db/schema/ai-chat.ts
+++ src/db/schema/ai-chat.ts
@@ -6,7 +6,8 @@ export const aiChat = pgTable("ai_chat", {
     id: uuid('id').primaryKey().defaultRandom(),
     chatId: text('chat_id').notNull(),
     title: varchar({length:300}),
-    admin: text("user_id").references(()=>user.id, {onDelete: 'cascade'}).notNull(), //the real admin/user whho created/started this aichat, i may be started the chat i may be not can be diff but it always show that who started it, in case if forked/shared i should be the user
+    admin: text("admin_id").references(()=>user.id, {onDelete: 'cascade'}).notNull(), //the real admin whho created/started this aichat, i may be started the chat i may be not can be diff but it always show that who started it, in case if pair programing some one should be the user who joind the chat and admin will be who strted it 
+    user: text("user_id").references(()=>user.id, {onDelete: 'cascade'}).notNull(), //chat belogs to the user
     type: text("role", {enum: ['admin', 'joined', 'forked'] }).notNull(),
     createdAt: timestamp("created_at").notNull(),
     updatedAt: timestamp("updated_at").notNull(),
