dda9062516973f540d4d75b4518b53ce9769a712
aditya-pushkar <adityapushkar850@gmail.com>
2025-07-08 20:49:23 +0530

    /c/[id] path added

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,18 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   
   const authToken = getSessionCookie(request);
-
-  if (!authToken && pathname === '/') {
+  
+  if (!authToken && (pathname === '/' || pathname.startsWith('/c/'))) {
     return NextResponse.redirect(new URL('/home', request.url))
   }
-
+  
   if (authToken && pathname === '/home') {
     return NextResponse.redirect(new URL('/', request.url))
   }
-
+  
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/', '/home'],
+  matcher: ['/', '/home', '/c/:path*'],
 }
