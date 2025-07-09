a794e507e041ada4d03d39f6d2fcaf602da61c1e
aditya-pushkar <adityapushkar850@gmail.com>
2025-07-09 13:40:57 +0530

    useStore; util and modulized the component

--- src/app/(chat)/components/message.tsx
+++ src/app/(chat)/components/message.tsx
@@ -10,6 +10,11 @@ import {
 import CopyToClipboard from "./copy-to-clipboard";
 import type { MessageProps } from "@/lib/types";
 
+import { extractFirstCodeBlock } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+
+import { addNewSandbox } from "@/store/sandbox";
 
 const UserCollapsedInput = ({ content }: { content: string }) => {
   const shouldCollapse = content.length > 110;
@@ -36,47 +41,84 @@ const UserCollapsedInput = ({ content }: { content: string }) => {
   );
 };
 
- // <div className="block h-6 w-6 rounded-[13px] bg-gradient-to-b from-secondary to-accent-foreground" />
+const UserMessage = ({ content }: { content: string }) => {
+  return (
+    <div className="flex flex-row items-center gap-1 ">
+      <div className="-mt-5 h-5 w-5 rounded-[13px] bg-gradient-to-b from-secondary to-accent-foreground" />
+      <div className="flex flex-col gap-4 flex-1 min-w-0">
+        <div className="flex flex-col items-baseline group">
+          <div className="rounded-[27px] max-w-[85%] sm:max-w-[70%] text-sm text-gray-200 shadow-sm">
+            <UserCollapsedInput content={content} />
+          </div>
+          <div className="w-[50rem] opacity-0 group-hover:opacity-100 transition-opacity duration-200  px-3 -mt-3 mb-1">
+            <CopyToClipboard text={content} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export const ChatMessage = ({ id, message, isStreaming }: MessageProps) => {
+const AssistantMessage = ({
+  content,
+  isStreaming,
+  changeWindowState
+}: {
+  content: string;
+  isStreaming: boolean;
+  changeWindowState: (state:boolean) => void
+}) => {
+  const {isCodeLoading, pre_code, code, post_code} = extractFirstCodeBlock(content, isStreaming)
+  return (
+    <>
+      <span className="px-0.5 font-semibold text-xl italic text-white -mt-1.5 ">
+        g
+      </span>
+      <div className="">
+        <div className="text-sm flex flex-col gap-3 group">
+          <Markdown remarkPlugins={[remarkGfm]}>{pre_code}</Markdown>
+          {code&&(
+            <Button
+            size="sm" 
+            variant={"outline"} 
+            disabled={isCodeLoading}
+            onClick={()=>{
+              addNewSandbox({id: '', code: code})
+              changeWindowState(true)
+            }}
+            className="w-fit py-4 font-light ring-2 ring-accent-foreground/80 my-3">
+             {isCodeLoading&&(<Loader2 className="text-green-500 animate-spin"/>)} Component
+            </Button>
+          )}
+          <Markdown remarkPlugins={[remarkGfm]}>{post_code}</Markdown>
+          
+          <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+            <div className="px-0.5 ">
+              <CopyToClipboard text={content} />
+            </div>
+          </div>
+        </div>
+        {isStreaming && "Loading..."}
+      </div>
+    </>
+  );
+};
+
+
+export const ChatMessage = ({ id, message, isStreaming, changeWindowStateTo }: MessageProps) => {
   return (
     <div className="px-2 sm:px-4" key={id}>
       <div className="max-w-xs sm:max-w-md md:max-w-2xl mx-auto flex gap-2 sm:gap-4">
         {/* User Message */}
-        {message.role === "user" && (
-          <div className="flex flex-row items-center gap-1 ">
-            <div className="-mt-5 h-5 w-5 rounded-[13px] bg-gradient-to-b from-secondary to-accent-foreground" />
-            <div className="flex flex-col gap-4 flex-1 min-w-0">
-              <div className="flex flex-col items-baseline group">
-                <div className="rounded-[27px] max-w-[85%] sm:max-w-[70%] text-sm text-gray-200 shadow-sm">
-                  <UserCollapsedInput content={message.content} />
-                </div>
-                <div className="w-[50rem] opacity-0 group-hover:opacity-100 transition-opacity duration-200  px-0.5 -mt-5 mb-1">
-                  <CopyToClipboard text={message.content} />
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+        {message.role === "user" && <UserMessage content={message.content} />}
 
         {/* Assistant Message */}
         {message.role === "assistant" && (
-          <>
-            <span className="px-0.5 font-semibold text-xl italic text-white -mt-1.5 ">
-              g
-            </span>
-            <div className="flex flex-col gap-2 flex-1 min-w-0">
-              <div className="leading-relaxed text-sm flex flex-col gap-4 group">
-                {message.content}
-                <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-                  <div className="px-0.5 -mt-2">
-                    <CopyToClipboard text={message.content} />
-                  </div>
-                </div>
-              </div>
-              {isStreaming&&"Loading..."}
-            </div>
-          </>
+          <AssistantMessage
+            content={message.content}
+            isStreaming={isStreaming}
+            changeWindowState={changeWindowStateTo}
+          />
         )}
       </div>
     </div>
