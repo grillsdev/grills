8c9f505b9fea2155121efa7ccde6b36355a9b6fa
aditya-pushkar <adityapushkar850@gmail.com>
2025-07-10 07:09:20 +0530

    ref.

--- src/store/sandbox.ts
+++ src/store/sandbox.ts
@@ -1,12 +1,4 @@
-import { atom, map } from 'nanostores'
+import { map } from 'nanostores'
 import { SandboxStoreInterface } from '@/lib/types';
-
-
-export const $sandbox = atom<SandboxStoreInterface>({id:'', code:'', isStreaming:false})
-
-
-export const updateSandboxStore = (sandbox:SandboxStoreInterface) => {
-    $sandbox.set(sandbox)
-}
 
 export const $sanboxObj = map<SandboxStoreInterface>({id:'', code:'', isStreaming:false})
