7395a6072ced70fe6b4a476398a950c6dd1df416
aditya-pushkar <adityapushkar850@gmail.com>
2025-07-14 07:09:55 +0530

    chat overview on bases of createdAt

--- src/app/api/completion/user/route.ts
+++ src/app/api/completion/user/route.ts
@@ -3,6 +3,7 @@ import { auth } from "@/lib/auth";
 import { db } from "@/db";
 import { aiChat } from "@/db/schema/ai-chat";
 
+// user Saved Project
 export async function GET(request: Request) {
     const session = await auth.api.getSession({
       headers: request.headers,
@@ -13,7 +14,7 @@ export async function GET(request: Request) {
     }
     try{
         const userId = session.user.id
-        const result = await db.select().from(aiChat).where(eq(aiChat.user, userId)).orderBy(desc(aiChat.updatedAt)).limit(20)
+        const result = await db.select().from(aiChat).where(eq(aiChat.user, userId)).orderBy(desc(aiChat.createdAt)).limit(20)
         return Response.json({"projects": result}, {status:200})
     }catch{
     return Response.json(
