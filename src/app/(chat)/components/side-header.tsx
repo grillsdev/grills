b06389eed9e6543771dcf9661e9eb17ce84ce619
aditya-pushkar <adityapushkar850@gmail.com>
2025-07-31 01:20:48 +0530

    manula sidebar btn trigger handling (still not working)

--- src/app/(chat)/components/side-header.tsx
+++ src/app/(chat)/components/side-header.tsx
@@ -1,12 +1,18 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 
 import { StartProjectDialogBtn } from "./start-project";
 import { Invitation } from "./invitation";
 export function SideHeader() {
+  const {setOpen, state} = useSidebar()
+
+  const handleSidebarState = () => {
+    const isOpen = state === "expanded"
+    setOpen(!isOpen)
+  }
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 py-1 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
+        <SidebarTrigger className="-ml-1" onClick={handleSidebarState} />
         <div className="ml-auto flex items-center gap-3">
           <div className="flex flex-row items-center gap-3 ">
             <StartProjectDialogBtn />
