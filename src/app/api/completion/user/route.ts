dc05c3b1519caf3f9795a5d8f2866ee3b51d34d8
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-03 23:11:14 +0530

    auth added for vishvakarma angine

--- src/app/api/completion/user/route.ts
+++ src/app/api/completion/user/route.ts
@@ -1,26 +1,51 @@
-import { eq, desc} from "drizzle-orm";
+import { eq, desc } from "drizzle-orm";
 import { auth } from "@/lib/auth";
 import { getDb } from "@/db";
 import { aiChat } from "@/db/schema/ai-chat";
 
 // user Saved Project
 export async function GET(request: Request) {
-    const session = await auth.api.getSession({
-      headers: request.headers,
-    });
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
 
-    if (!session) {
-      return Response.json({ error: "Unauthorized" }, { status: 401 });
-    }
-    try{
-      const db = await getDb()
-        const userId = session.user.id
-        const result = await db.select().from(aiChat).where(eq(aiChat.user, userId)).orderBy(desc(aiChat.createdAt)).limit(20)
-        return Response.json({"projects": result}, {status:200})
-    }catch{
+  if (!session) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const db = await getDb();
+    const userId = session.user.id;
+    const result = await db
+      .select()
+      .from(aiChat)
+      .where(eq(aiChat.user, userId))
+      .orderBy(desc(aiChat.createdAt))
+      .limit(20);
+    return Response.json({ projects: result }, { status: 200 });
+  } catch {
     return Response.json(
       { error: "Failed to process request" },
       { status: 500 }
     );
-    }
+  }
+}
+
+// for vishvakarma engine auth
+export async function POST(request: Request) {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  if (!session) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const user = session.user;
+    return Response.json({ user: user }, { status: 200 });
+  } catch {
+    return Response.json(
+      { error: "Failed to process request" },
+      { status: 500 }
+    );
+  }
 }
