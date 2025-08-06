a73600e9d96506cf561871122650e1743fd29b08
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-07 02:13:40 +0530

    rmd get join notification

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -4,8 +4,6 @@ import { v4 as uuid } from "uuid";
 
 import { aiChat } from "@/db/schema/ai-chat";
 import { getDb } from "@/db";
-import { auth } from "@/lib/auth";
-import Redis from "ioredis";
 
 // create a project -> start a chat
 export const POST = authMiddleware(async (request: Request, session) => {
@@ -29,7 +27,6 @@ export const POST = authMiddleware(async (request: Request, session) => {
       chatId,
       title: title.trim(),
       admin: session.userId,
-      user: session.userId,
       type: "admin",
       createdAt: new Date(),
       updatedAt: new Date(),
@@ -68,84 +65,3 @@ export const POST = authMiddleware(async (request: Request, session) => {
     );
   }
 });
-
-
-// get the join notifictions to the admin
-// Move this to notification -> /api/project/[id]/notification
-export async function GET(request: Request) {
-  try{
-     const session = await auth.api.getSession({
-     headers: request.headers,
-    });
-
-    if (!session) {
-        return Response.json(
-        { error: "Unauthorized" }, 
-        { status: 401 }
-        );
-    }
-    const adminId = session.user.id
-    const setKey = `join:request:${adminId}`;
-
-    const redisSubscriber = new Redis(process.env.UPSTASH_REDIS_URL!);
-    const encoder = new TextEncoder();
-    let isStreamActive = true;
-
-    const completionStream = new ReadableStream({
-      start(controller) {
-        redisSubscriber.subscribe(setKey, (err) => {
-          if (err) {
-            console.error('Redis subscribe error:', err);
-            controller.error(err);
-            return;
-          }
-          console.log(`Subscribed to ${setKey}`);
-        });
-
-        // Listen for new Completion from upstash Redis
-        redisSubscriber.on('message', (channel, message) => {
-          if (!isStreamActive) return;
-          
-          try {
-            if (channel === setKey) {
-              controller.enqueue(encoder.encode(`data: ${message}\n\n`));
-            }
-          } catch (error) {
-            console.error('Error in message handler:', error);
-          }
-        });
-
-        redisSubscriber.on('error', (error) => {
-          console.error('Redis error:', error);
-          if (isStreamActive) {
-            controller.error(error);
-          }
-        });
-      },
-      
-      cancel() {
-        isStreamActive = false;
-        try {
-          redisSubscriber.unsubscribe(setKey);
-          redisSubscriber.quit();
-        } catch (error) {
-          console.error('Error during cleanup:', error);
-        }
-      }
-    });
-
-    return new Response(completionStream, {
-      headers: {
-        'Content-Type': 'text/event-stream; charset=utf-8',
-        'Cache-Control': 'no-cache, no-transform',
-        'Connection': 'keep-alive',
-        'Content-Encoding': 'none',
-    },
-    })
-   
-  }catch(error){
-    console.error('SSE error:', error);
-    return new Response('Internal server error', { status: 500 });
-  }
-
-}
