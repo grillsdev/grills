98e6a25c129f44e1914a2d5d82a30bc98829009d
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-07 02:12:56 +0530

    added auth to the chat history

--- src/app/api/completion/user/chat/route.ts
+++ src/app/api/completion/user/chat/route.ts
@@ -4,24 +4,36 @@
 
 
 import { NextRequest } from "next/server";
-import { Redis as UpstashR } from '@upstash/redis';
+import { Redis } from '@upstash/redis';
+import { auth } from "@/lib/auth";
+import { getDb } from "@/db";
+import { aiChat } from "@/db/schema/ai-chat";
+import { eq, and } from "drizzle-orm";
 
-const redis = new UpstashR({
+const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
   keepAlive:true
 });
 
 export async function GET(request:NextRequest){
+  const session = await auth.api.getSession({
+      headers: request.headers,
+    });
+  
+    if (!session) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
   try{
     const searchParam =  request.nextUrl.searchParams;
     const chatId = searchParam.get("chatId")
-
     if(!chatId) return Response.json(
       "Please navigate to Valid Chat Page" , 
       { status: 500 }
     );
-
+    const db = await getDb();
+    const chatAdmin = await db.select().from(aiChat).where(and(eq(aiChat.chatId, chatId), eq(aiChat.admin, session.user.id)))
+    if(!chatAdmin[0]) return Response.json({error: "Not found"}, {status: 404})
     const setKey = `chat:${chatId}:messages`
     const getUserChats = await redis.lrange(setKey, 0, -1)
     // got newst to oldest message but we want olders to newest for chat ui
