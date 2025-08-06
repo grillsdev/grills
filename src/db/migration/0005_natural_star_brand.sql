81eb0f20ed645faf9456209f12a0688be925818d
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-07 02:09:29 +0530

    rmd user and joined enum

---
+++ src/db/migration/0005_natural_star_brand.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "ai_chat" DROP CONSTRAINT "ai_chat_user_id_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "ai_chat" DROP COLUMN "user_id";
--- src/db/schema/ai-chat.ts
+++ src/db/schema/ai-chat.ts
@@ -7,8 +7,7 @@ export const aiChat = pgTable("ai_chat", {
     chatId: text('chat_id').notNull(),
     title: varchar({length:300}),
     admin: text("admin_id").references(()=>user.id, {onDelete: 'cascade'}).notNull(), //the real admin whho created/started this aichat, i may be started the chat i may be not can be diff but it always show that who started it, in case if pair programing some one should be the user who joind the chat and admin will be who strted it 
-    user: text("user_id").references(()=>user.id, {onDelete: 'cascade'}).notNull(), //chat belogs to the user
-    type: text("role", {enum: ['admin', 'joined', 'forked'] }).notNull(),
+    type: text("role", {enum: ['admin', 'forked'] }).notNull(),
     createdAt: timestamp("created_at").notNull(),
     updatedAt: timestamp("updated_at").notNull(),
 })
