8dc3b253658d5bebbafbb7f8423b4b709e1749ee
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-07 10:51:13 +0530

    added openai supper

--- src/app/api/theme/route.ts
+++ src/app/api/theme/route.ts
@@ -4,7 +4,6 @@ import { eq, desc } from "drizzle-orm";
 import { generateObject } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
 
-
 import { themeVerificationPrompt } from "@/lib/prompt";
 import { CreateThemeContext, LLMProvider, themeSchema } from "@/lib/types";
 import { getDb } from "@/db";
@@ -12,12 +11,12 @@ import { userTheme } from "@/db/schema/theme";
 import { LLMsOpenAICompatibleEndpoint } from "@/lib/utils";
 
 /**
- * 
+ *
  * add the switch case for all other llms
  */
 export async function POST(request: Request) {
   const session = await auth.api.getSession({
-            headers: request.headers,
+    headers: request.headers,
   });
   if (!session) {
     return Response.json({ error: "Unauthorized" }, { status: 401 });
@@ -30,28 +29,36 @@ export async function POST(request: Request) {
     baseURL: LLMsOpenAICompatibleEndpoint[context.llm as LLMProvider],
   });
   try {
+    let model;
+    switch (context.llm) {
+      case "openrouter":
+        model = "x-ai/grok-3-mini";
+        break;
+      default:
+        model = "o4-mini";
+    }
     const { object } = await generateObject({
-      model: operator.chat("x-ai/grok-3-mini"),
+      model: operator.chat(model),
       schema: themeSchema,
       system: themeVerificationPrompt,
       prompt: `Validate this: ${context.content}`,
     });
+    console.log(object);
     if (object.isValid) {
-      await db
-        .insert(userTheme)
-        .values({
-          name: object.name,
-          color: object.color,
-          data: object.data,
-          createdAt: new Date(),
-          user: userId,
-        });
+      await db.insert(userTheme).values({
+        name: object.name,
+        color: object.color,
+        data: object.data,
+        createdAt: new Date(),
+        user: userId,
+      });
       return Response.json(object, { status: 200 });
     }
     return Response.json("Not enough credit left in user's API", {
       status: 400,
     });
-  } catch {
+  } catch (ee) {
+    console.log("!!!EERR!!", ee);
     return Response.json("Not enough credit left in user's API", {
       status: 500,
     });
@@ -60,12 +67,12 @@ export async function POST(request: Request) {
 
 export async function GET(request: Request) {
   const session = await auth.api.getSession({
-            headers: request.headers,
+    headers: request.headers,
   });
   if (!session) {
     return Response.json({ error: "Unauthorized" }, { status: 401 });
   }
-  const userId = session.user.id
+  const userId = session.user.id;
   const db = await getDb();
   const savedThemes = await db
     .select({
@@ -79,4 +86,4 @@ export async function GET(request: Request) {
     .where(eq(userTheme.user, userId))
     .orderBy(desc(userTheme.createdAt));
   return Response.json(savedThemes, { status: 200 });
-};
+}
