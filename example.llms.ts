7afbfb7bcd324a2ff6c4b6db88b073db238ed992
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-30 14:34:14 +0530

    more llm and model ex

--- example.llms.ts
+++ example.llms.ts
@@ -14,4 +14,11 @@ export const models = [
     {id:"default", title: "Qwen3 Coder", name: "qwen/qwen3-coder", llm_id: "openrouter-primarykey", created_at: "Select now"},
     {id:"default", title: "GPT 5 Mini", name: "openai/gpt-5-mini", llm_id: "openrouter-primarykey", created_at: "Select now"},
     {id:"default", title: "G 2.5 flash lite", name: "google/gemini-2.5-flash-lite", llm_id: "openrouter-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT-5 Mini", name: "gpt-5-mini-2025-08-07", llm_id: "openai-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT-5 Nano", name: "gpt-5-nano-2025-08-07", llm_id: "openai-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT-4.1", name: "gpt-4.1", llm_id: "openai-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT-4.1 Mini", name: "gpt-4.1-mini", llm_id: "openai-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT 4o", name: "gpt-4o", llm_id: "openai-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT o4 Mini", name: "o4-mini", llm_id: "openai-primarykey", created_at: "Select now"},
+    {id:"default", title: "GPT o3 Mini ", name: "o3-mini", llm_id: "openai-primarykey", created_at: "Select now"},
 ]
