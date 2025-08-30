8622e514c7cf6c0e85c2aa8e7d764a51e618c1d4
aditya-pushkar <adityapushkar850@gmail.com>
2025-08-31 01:20:55 +0530

    add some more example

--- example.llms.ts
+++ example.llms.ts
@@ -14,6 +14,7 @@ export const models = [
     {id:"default", title: "Qwen3 Coder", name: "qwen/qwen3-coder", llm_id: "openrouter-primarykey", created_at: "Select now"},
     {id:"default", title: "GPT 5 Mini", name: "openai/gpt-5-mini", llm_id: "openrouter-primarykey", created_at: "Select now"},
     {id:"default", title: "G 2.5 flash lite", name: "google/gemini-2.5-flash-lite", llm_id: "openrouter-primarykey", created_at: "Select now"},
+    {id:"default", title: "Grok Code Fast 1", name: "x-ai/grok-code-fast-1", llm_id: "openrouter-primarykey", created_at: "Select now"},
     {id:"default", title: "GPT-5 Mini", name: "gpt-5-mini-2025-08-07", llm_id: "openai-primarykey", created_at: "Select now"},
     {id:"default", title: "GPT-5 Nano", name: "gpt-5-nano-2025-08-07", llm_id: "openai-primarykey", created_at: "Select now"},
     {id:"default", title: "GPT-4.1", name: "gpt-4.1", llm_id: "openai-primarykey", created_at: "Select now"},
