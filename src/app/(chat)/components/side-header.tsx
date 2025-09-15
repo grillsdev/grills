2783945919184ad2bff31f64354823f91d389275
aditya-pushkar <adityapushkar850@gmail.com>
2025-09-15 18:22:26 +0530

    rmd StartProjectBtn and added go to new chat btn

--- src/app/(chat)/components/side-header.tsx
+++ src/app/(chat)/components/side-header.tsx
@@ -1,8 +1,7 @@
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
-
-import { StartProjectDialogBtn } from "./start-project";
 import { Button } from "@/components/ui/button";
-
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export function SideHeader() {
   const {setOpen, state} = useSidebar()
@@ -14,13 +13,18 @@ export function SideHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 py-1 lg:gap-2 lg:px-6">
+        <div className="flex flex-row items-center gap-2">
         <SidebarTrigger className="-ml-1" onClick={handleSidebarState} />
+        {state==="collapsed"&&(
+        <Link href="/"><Plus/></Link>
+        )}
+        </div>
         <div className="ml-auto flex items-center gap-3">
           <div className="flex flex-row items-center gap-1 ">
             <Button size={"sm"} variant={"link"} className="text-primary text-xs">
               <a href="https://grills.featurebase.app" target="_blank">Feedback</a>
             </Button>
-            <StartProjectDialogBtn />
+
           </div>
         </div>
       </div>
